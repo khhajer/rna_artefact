46ab6309d290c1345936d4dc513862642cd44b14
hajer khalfaoui <khalfaouihajer99@gmail.com>
2023-10-03 15:56:45 +0000

    Update file Launch_RNA_Artefact.sh

--- Launch_RNA_Artefact.sh
+++ Launch_RNA_Artefact.sh
@@ -1,14 +1,4 @@
 #!/bin/bash 
-<<<<<<< HEAD
-module load conda 
-rm -r -f Raw_variant
-source activate RNA_Artefact
-snakemake -s snakefile --unlock
-snakemake --cleanup-metadata snakefile
-echo "Launch RNA artefact analysis "
-snakemake -F  -s snakefile  --configfile "/trinity/home/hkhalfaoui/pipeline/rna_artefact/config.yaml"  --cluster-config cluster_config.yml  --cluster "sbatch  --nodes {cluster.nodes} --ntasks {cluster.ntasks} --cpus-per-task {cluster.cpus-per-task} "  --jobs 200 --dry-run
-#snakemake -F  -s snakefile  --configfile "/trinity/home/hkhalfaoui/pipeline/rna_artefact/config.yaml" --cluster-config cluster_config.yml  --cluster "sbatch  --nodes {cluster.nodes} --ntasks {cluster.ntasks} --cpus-per-task {cluster.cpus-per-task} "  --notemp --jobs 200
-=======
 #rm -r -f Raw_variant
 # Define code color 
 terminalColorError='\033[1;31m'
@@ -48,5 +38,5 @@ else
     #snakemake -F  -s snakefile -c1 
 
 fi
->>>>>>> Slurm_development
+
 conda deactivate 
